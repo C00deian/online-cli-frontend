0a560aec2830027bd7b694b0285909c7d1efbd76
Gyan <gyanendrayadav12345789@gmail.com>
2025-08-07 15:09:08 +0530

    figma is live now

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,1 @@
-import {  Routes, Route } from "react-router-dom";
-
-import TerminalClient from "./components/TerminalClient"; // ✅ Make sure path is correct
-import Home from "./components/Home";
-function App() {
-  return (
-  
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/terminal" element={<TerminalClient />} />
-      </Routes>
-  
-  );
-}
-
-export default App;
+ 
